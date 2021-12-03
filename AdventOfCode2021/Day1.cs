faa1f9708a53f6ca2e11b458b63a1e3974292401
Mohammed Souleymane <Mohammedsouleymane@outlook.com>
2021-12-03 13:39:49 +0100

    fors to linq expressions

--- AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/Day1.cs
@@ -5,14 +5,19 @@
         private static string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent + "/Input/Day1/Input.txt";
         public static void One()
         {
-            List<int> list = File.ReadAllLines(path).Select(x=> int.Parse(x)).ToList();
-            Console.WriteLine(Enumerable.Range(1, list.Count - 1).Where(i => list[i] > list[i - 1]).ToList().Count);
+            List<int> list = File.ReadAllLines(path).Select(x => int.Parse(x)).ToList();
+            Console.WriteLine(Enumerable.Range(1, list.Count - 1)
+                .Where(i => list[i] > list[i - 1])
+                .ToList().Count);
         }
 
         public static void Two()
         {
-            List<int> list = File.ReadAllLines(path).Select(x => int.Parse(x)).ToList();            
-            Console.WriteLine(Enumerable.Range(1, list.Count - 2).Where(i => list.Count > i + 1 && list.Count > i + 2 && list[i] + list[i +1] + list[i + 2] > list[i-1] + list[i] + list[i +1]).ToList().Count);
+            List<int> list = File.ReadAllLines(path).Select(x => int.Parse(x)).ToList();
+            Console.WriteLine(Enumerable
+                .Range(1, list.Count - 2).
+                Where(i => list.Count > i + 1 && list.Count > i + 2 && list[i] + list[i + 1] + list[i + 2] > list[i - 1] + list[i] + list[i + 1])
+                .ToList().Count);
 
             //List<int> result = new();
             //for (int i = 0; i < list.Count - 2; i++)
--- AdventOfCode2021/Day3.cs
+++ AdventOfCode2021/Day3.cs
@@ -11,19 +11,20 @@ namespace AdventOfCode2021
         private static string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent + "/Input/Day3/Input.txt";
         public static void One()
         {
-            String input;
             var binary = File.ReadAllLines(path).ToList();
             string mostFrequent = "";
             string leastFrequent = "";
             for (int i = 0; i < binary[0].Length; i++)
             {
-                List<int> numbers = new();
-                for (int j = 0; j < binary.Count; j++)
-                {
-                    numbers.Add(int.Parse(binary[j][i].ToString()));
-                }
-                mostFrequent += numbers.GroupBy(n => n).OrderByDescending(g => g.Count()).First().Key;
-                leastFrequent += numbers.GroupBy(n => n).OrderBy(g => g.Count()).First().Key;
+                List<int> numbers = binary.Select(x => int.Parse(x[i].ToString())).ToList();
+                mostFrequent += numbers
+                    .GroupBy(n => n)
+                    .OrderByDescending(g => g.Count())
+                    .First().Key;
+                leastFrequent += numbers
+                    .GroupBy(n => n)
+                    .OrderBy(g => g.Count())
+                    .First().Key;
             }
 
             Console.WriteLine(Convert.ToInt32(mostFrequent, 2) * Convert.ToInt32(leastFrequent, 2));
@@ -31,34 +32,33 @@ namespace AdventOfCode2021
 
         public static void Two()
         {
-            String input;
             var binary = File.ReadAllLines(path).ToList();
             List<string> oxygenGngRating = new(binary);
             List<string> co2ScrabberRating = new(binary);
 
             for (int i = 0; i < binary[0].Length; i++)
             {
-                string mostFrequent = "";
-                string leastFrequent = "";
-                List<int> numbers = new();
+                List<int> numbers = co2ScrabberRating
+                    .Select(x => int.Parse(x[i].ToString()))
+                    .ToList();
+                string leastFrequent = numbers
+                    .GroupBy(n => n)
+                    .OrderBy(g => g.Count())
+                    .First().Key.ToString();
 
-
-                for (int j = 0; j < co2ScrabberRating.Count; j++)
-                {
-                    numbers.Add(int.Parse(co2ScrabberRating[j][i].ToString()));
-                }
-
-                leastFrequent = numbers.GroupBy(n => n).OrderBy(g => g.Count()).First().Key.ToString();
                 if (co2ScrabberRating.Count > 1)
                     co2ScrabberRating = co2ScrabberRating.Where(b => b[i] == leastFrequent[0]).ToList();
 
-                numbers = new List<int>();
-                for (int j = 0; j < oxygenGngRating.Count; j++)
-                {
-                    numbers.Add(int.Parse(oxygenGngRating[j][i].ToString()));
-                }
+                numbers = oxygenGngRating
+                    .Select(x => int.Parse(x[i].ToString()))
+                    .ToList();
 
-                mostFrequent = numbers.GroupBy(n => n).OrderByDescending(g => g.Count()).ThenByDescending(g => g.Key).First().Key.ToString();
+                string mostFrequent = numbers
+                    .GroupBy(n => n)
+                    .OrderByDescending(g => g.Count())
+                    .ThenByDescending(g => g.Key)
+                    .First().Key.ToString();
+
                 if (oxygenGngRating.Count > 1)
                     oxygenGngRating = oxygenGngRating.Where(b => b[i] == mostFrequent[0]).ToList();
             }

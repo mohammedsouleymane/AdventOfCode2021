0637f64657099cc936da92c1ce7a25736157273a
Mohammed Souleymane <Mohammedsouleymane@outlook.com>
2021-12-09 16:31:16 +0100

    Day 9 update

--- AdventOfCode2021/Day9.cs
+++ AdventOfCode2021/Day9.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2021
+﻿namespace AdventOfCode2021
 {
     internal class Day9
     {
@@ -13,8 +7,8 @@ namespace AdventOfCode2021
         {
             var input = File.ReadAllLines(path);
 
-            var matrix = new int[input.Count(), input[0].Length];
-            var lows = new List<int>();
+            var matrix = new int[input.Length, input[0].Length];
+            var lowpoints = new List<int>();
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -44,21 +38,20 @@ namespace AdventOfCode2021
 
                     if (matrix[i, j] < up && matrix[i, j] < down && matrix[i, j] < left && matrix[i, j] < right)
                     {
-                        lows.Add(matrix[i, j]);
+                        lowpoints.Add(matrix[i, j]);
                     }
 
                 }
             }
-            Console.WriteLine(lows.Sum() + lows.Count());
+            Console.WriteLine(lowpoints.Sum() + lowpoints.Count);
         }
 
         public static void Two()
         {
             var input = File.ReadAllLines(path);
-
-            var matrix = new int[input.Count(), input[0].Length];
+            var matrix = new int[input.Length, input[0].Length];
             var bassins = new List<int>();
-            
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
@@ -89,7 +82,6 @@ namespace AdventOfCode2021
                     {
                         bassins.Add(BassinSize(matrix, i, j));
                     }
-
                 }
                 bassins.Sort();
                 bassins.Reverse();
@@ -102,28 +94,20 @@ namespace AdventOfCode2021
             if (matrix[y, x] == 9)
                 return 0;
             matrix[y, x] = -1;
-            int sum = 1;
+            int bassinSize = 1;
             if (x > 0 && matrix[y, x - 1] > -1 && matrix[y, x - 1] < 9)
-                sum += BassinSize(matrix, y, x - 1);
-            
+                bassinSize += BassinSize(matrix, y, x - 1);
 
             if (y > 0 && matrix[y - 1, x] > -1 && matrix[y - 1, x] < 9)
-                sum += BassinSize(matrix, y - 1, x);
-            
+                bassinSize += BassinSize(matrix, y - 1, x);
 
             if (y < matrix.GetLength(0) - 1 && matrix[y + 1, x] > -1 && matrix[y + 1, x] < 9)
-                sum  += BassinSize(matrix, y + 1, x);
-            
-
+                bassinSize += BassinSize(matrix, y + 1, x);
 
             if (x < matrix.GetLength(1) - 1 && matrix[y, x + 1] > -1 && matrix[y, x + 1] < 9)
-                sum += BassinSize(matrix, y, x + 1);
-            
-           
+                bassinSize += BassinSize(matrix, y, x + 1);
 
-            return  sum;
+            return bassinSize;
         }
-
-
     }
 }
